f09e82bf227da0ea6b416c1fce8e47affeb47a18
Michael Bower <mikey.bower93@gmail.com>
2020-03-14 14:21:00 +0000

    Delete unnecessary files.

--- lib/json_api_ecto_builder.ex
+++ lib/json_api_ecto_builder.ex
@@ -1,7 +1,14 @@
+#TODO: this probably just wants to be JsonApiEctoBuilder
+#TODO: finish documentation
+#TODO: delete json_api_ecto_builder or move this to there?
 defmodule JsonApiEctoBuilder do
   @moduledoc """
   Documentation for JsonApiEctoBuilder.
   """
+
+  import Ecto.Query
+
+  alias JsonApiEctoBuilder.Applier.{Filter,Join,Sort,Include}
 
   @doc """
   Hello world.
@@ -12,7 +19,24 @@ defmodule JsonApiEctoBuilder do
       :world
 
   """
-  def hello do
-    :world
+  def build_query(base_query, base_type, base_alias, params, apply_join_callback) do
+    [primary_key] = base_type.__schema__(:primary_key)
+
+    #TODO: they must! prebuild the request - test
+    #TODO: unit tests
+    #TODO: suggest skriviner for pagination, why reinvent a good horse, and ja_serializer for view renders etc
+    #TODO: comment functions
+    #TODO: can probably use https://hexdocs.pm/plug/Plug.Parsers.html to build the query for unit tests
+    #TODO: finish cleaning up, test in rocket library, put in github, start unit testing heavily, documentation for hex
+    sub_query =
+      base_query
+      |> Join.apply(params, apply_join_callback)
+      |> Filter.apply(params, base_alias)
+      |> Sort.apply(params, base_alias, primary_key)
+
+    base_type
+    |> join(:inner, [b], s in subquery(sub_query), on: b.id == field(s, ^primary_key))
+    |> order_by([_, x], x.ordinal)
+    |> Include.apply(params)
   end
 end
